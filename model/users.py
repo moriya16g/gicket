744800349c825a65cf00764b26c7a71a1d09e2fa
Shigeru Moriya <moriya@sarotti.net>
2017-08-31 00:08:49 +0900

    add User class and Users class.

---
+++ model/users.py
@@ -0,0 +1,45 @@
+"""
+Module handling user information.
+"""
+
+class User:
+    """
+    Class representing a user.
+    """
+
+    def __init__(self, name, mail_address):
+        """
+        Constructor.
+        Arguments:
+          name :: name used by Git
+          mail_address :: mail address used by Git
+        """
+        self.name = name
+        self.mail = mail_address
+
+        
+class Users:
+    """
+    Class representing collection of users.
+    """
+
+    def __init__(self):
+        """
+        Constructor.
+        """
+        self.users = []
+
+    def add_user(self, user):
+        """
+        Add a user instans.
+        Do not register users with duplicate mail addresses.
+        Arguments:
+          user :: instans of User class
+        Return Values:
+          True :: add user
+          False :: do not add
+        """
+        for i in range(0, len(users) - 1):
+            if users[i].mail == user.mail:
+                return False
+        users.append(user)
